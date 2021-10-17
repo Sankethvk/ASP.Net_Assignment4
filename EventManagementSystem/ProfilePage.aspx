b98b8a2e6eba8f3d358b9d84aa7bc179e7e77b46
SanketV <sankethvivek@gmail.com>
2021-10-17 14:22:35 +0530

    Without CSS and bootstrap

---
+++ EventManagementSystem/ProfilePage.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Trace="true" Title="" Language="C#" MasterPageFile="~/eventmanagement.Master" AutoEventWireup="true" CodeBehind="ProfilePage.aspx.cs" Inherits="EventManagementSystem.ProfilePage" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h2>My Profile</h2>
+    <p>Name&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Label ID="lblname" runat="server" Text="Label"></asp:Label>
+    </p>
+    <p>EmailID&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Label ID="lblemail" runat="server" Text="Label"></asp:Label>
+    </p>
+    <p>Phone&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Label ID="lblphone" runat="server" Text="Label"></asp:Label>
+    </p>
+    <p>Address&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Label ID="lbladdress" runat="server" Text="Label"></asp:Label>
+    </p>
+    <p>Events&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="btnevents" runat="server" ForeColor="#3333FF" OnClick="btnevents_Click" Text="Events" />
+    </p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+</asp:Content>
---
+++ EventManagementSystem/UserRegistration.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.Entity;
+using EventBusiness;
+using EventManagementDataLayer;
+using Tulpep.NotificationWindow;
+
+namespace EventManagementSystem
+{
+    public partial class UserRegistration : System.Web.UI.Page
+    {
+        BusinessLayerClass businesslayerobj = new BusinessLayerClass();
+        UsersTable userdataobj = new UsersTable();
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void txtusersubmit_Click(object sender, EventArgs e)
+        {
+            userdataobj.first_name = txtuserfirstname.Text;
+            userdataobj.last_name = txtuserlastname.Text;
+            userdataobj.email = txtuseremailid.Text;
+            userdataobj.password = txtuserpassword.Text;
+            userdataobj.phone = Convert.ToInt32(txtuserphone.Text);
+            userdataobj.address = txtuseraddress.Text;
+            string msg = businesslayerobj.InsertUser(userdataobj);
+            //Response.Write(msg);
+            PopupNotifier popup = new PopupNotifier();
+            popup.TitleText = "Congrats";
+            popup.ContentText = "Account has been created successfully";
+            popup.Popup();
+            Response.Redirect("LoginPage.aspx");
+
+
+        }
+    }
+}
